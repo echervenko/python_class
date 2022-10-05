001b62952ed7ad5378032e193f31e7c9e0b39064
somename <a@gmail.com>
2022-10-05 15:57:09 +0000

    added conditions

---
+++ class3/conditions.py
@@ -0,0 +1,30 @@
+#is
+
+# NUMBER1 = 10
+# NUMBER2 = 10
+
+# if NUMBER1 is NUMBER2:
+#     print("they're equal")
+#     else:
+#         print("they're not equal")
+# if NUMBER1 is not NUMBER2: 
+#     print("they're not equal")
+# else:
+#     print("they're equal")
+
+#in
+HIDDEN_FRUIT = "kiwi"
+
+FRUIT_LIST1 = ["apple", "pear", "banana"]
+print(type(FRUIT_LIST1))
+FRUIT_LIST2 = ("mango", "melon", "kiwi")
+FRUIT_LIST3 = {"peach", "apricot", "cherry"}
+
+if HIDDEN_FRUIT is in FRUIT_LIST1
+    print("hidden fruit is in list1")
+elif HIDDEN_FRUIT in FRUIT_LIST2:
+    print("hidden fruit is in list2")
+elif HIDDEN_FRUIT in FRUIT_LIST3:
+    print("hidden fruit is in list3")
+else:
+    print("hidden fruit is not found")
---
+++ class3/conditions2.py
@@ -0,0 +1,21 @@
+# == equals
+# != not equal
+# > greater than
+# < less than
+#>= greater than or equal to
+#<= less than or equal to
+
+
+#input function gets the response from the user at CLI
+
+# RESPONSE = input("when are you born?: ")
+# print(RESPONSE)
+
+# if RESPONSE >= "2000" and RESPONSE <= "2022":
+#     print("you were born in 21st century")
+# elif RESPONSE >= "1900" and RESPONSE <= "1999":
+#     print("u were born in 20th century")
+# elif RESPONSE == "1899" or RESPONSE <= "1800":
+#     print("u should not be runnung this")
+# else:
+#         print("pls give me a number")
